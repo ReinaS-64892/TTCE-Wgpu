8a4807348273c64533b8e72f64ea316bd5cc7e3f
Reina_Sakiria <rs@rs64.net>
2024-11-30 15:56:55 +0900

    chore: passthrough RequestDevicePreference

--- TTCE-Wgpu.Tests/Main.cs
+++ TTCE-Wgpu.Tests/Main.cs
@@ -1,17 +1,21 @@
 using net.rs64.TexTransCore;
+using net.rs64.TexTransCore.MultiLayerImageCanvas;
 using net.rs64.TexTransCoreEngineForWgpu;
 using net.rs64.TexTransCoreEngineForWgpu.Tests;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
 
 
 Console.WriteLine("Test Run");
 
 
-using var engineTest = new TestTTCEWgpuEngine(TexTransCoreTextureFormat.Byte, true);
+using var engineTest = new TestTTCEWgpuEngine(TexTransCoreTextureFormat.Byte, true, TTCEWgpuDevice.RequestDevicePreference.IntegratedGPUOrCPU);
 var ctx = engineTest.GetCtx();
 
-// using var rt = ctx.CreateRenderTexture(64, 256);
+// {
+// using var rt = ctx.CreateRenderTexture(256, 256, TexTransCoreTextureChannel.R);
 
-// ctx.ColorFill(rt, new(1, 1, 1, 1));
+// ctx.FillR(rt, 1f);
 
 // Span<byte> outByte = stackalloc byte[rt.Width * rt.Hight * 4];
 // ctx.DownloadTexture(outByte, TexTransCoreTextureFormat.Byte, rt);
@@ -19,4 +23,23 @@ var ctx = engineTest.GetCtx();
 // foreach (var v in outByte)
 // {
 //     Console.Write(v);
+// }
+// }
+
+// {
+//     var rt = ctx.CreateRenderTexture(2048, 2048);
+
+//     var imageBuf = new byte[rt.Width * rt.Hight * EnginUtil.GetPixelParByte(TexTransCoreTextureFormat.Byte, rt.ContainsChannel)];
+//     using (var img = Image.Load<Rgba32>(@"D:\Rs\TTCE-Wgpu\TTCE-Wgpu.Tests\TestData\0-Hair.png"))
+//         img.CopyPixelDataTo(imageBuf);
+
+//     ctx.UploadTexture<byte>(rt, imageBuf.AsSpan(), TexTransCoreTextureFormat.Byte);
+
+    // var selectiveColor = new SelectiveColorAdjustment(new(), new(), new(), new(), new(), new(), new(), new(), new(0,0,0,-0.3f), false);
+//     selectiveColor.GrabBlending(ctx, rt);
+
+//     ctx.DownloadTexture<byte>(imageBuf, TexTransCoreTextureFormat.Byte, rt);
+//     using (var outImage = Image<Rgba32>.LoadPixelData<Rgba32>(imageBuf.AsSpan(), 2048, 2048))
+//     using (var fsStream = File.Open(@"D:\Rs\TTCE-Wgpu\TTCE-Wgpu.Tests\TestData\0-Result.png", FileMode.OpenOrCreate))
+//         outImage.SaveAsPng(fsStream);
 // }
